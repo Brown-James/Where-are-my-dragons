82811b546768a482ddde80018db9771c827c0eb1
James Brown <james---b@hotmail.co.uk>
2014-08-17 23:40:58 +0100

    Image now reverts to the original on mouse enter

--- common.js
+++ common.js
@@ -1,26 +1,58 @@
+// Check if the page has any <img> tags
+	// If so....
+	// Find image - Append a special class chromeExtensionDragon.
+	// Play the sound.
+	// On hover of image - wait a few seconds. Insert animation at later date.
+	// Return the image to it's original source.
+	// Increment total dragons found counter.
+
+// TO DO
+//	Preserve Image Size
+//  Get correct audio clip
+//  Animation on hover
+//  Sound on hover?
+//  Total dragons found tracker
+
+
+
+
+
 dragonSrc = chrome.extension.getURL("dragon.png");
+
 // Get a HTML collection of all the images on the page.
 var imageList = document.images;
+
 // Create an array so we can access the .length method, and the randomly select a value.
 var imageArray = jQuery.makeArray(imageList);
 var imageChange = getRandomNumber(1, imageArray.length + 1);
-// Set the randomly chosen image to be the dragon.
-originalImageSrc = imageList[imageChange].src;
+
+// Set the randomly chosen image to be the dragon. Store the original source.
+var originalImageSrc = imageList[imageChange].src;
+console.log("Original Image Source: " + originalImageSrc);
 imageList[imageChange].src = dragonSrc;
+imageList[imageChange].className += " " + "dragonImage";
 
 $(document).ready(function(){
-	$("img[src$="+ dragonSrc).click(function(){
-		console.log("Dragon clicked!");
-		imageList[imageChange].src = originalImageSrc;
+	$(".dragonImage").ready(function(){
+		$(this).css("pointer-events", "none");
+		console.log("Pointer Events Removed!");
+	});
+	$(".dragonImage").mouseenter(function(){
+		console.log(originalImageSrc);
+		$(".dragonImage").attr('src', originalImageSrc);
+		console.log("Mouse Entered!");
 	});
 });
 
 var audio = null;
+
 // A helper function to generate a random integer (Min - Inclusive, Max - Exclusive)
 function getRandomNumber(Min, Max){
 	return Math.floor(Math.random() * (Max - Min) + Min);
 };
 
+// Provide the sound files name and extension in a string
+// Automatically plays the sound provided.
 function playSound(soundFile){
 	audio = document.createElement('audio');
 	document.body.appendChild(audio);
